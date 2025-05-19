a0be46d19f2fa3df9f82306d09f52d28c913452e
goldy <goldybhardwaj9868@gmail.com>
2025-05-19 15:39:25 +0530

    fixed footer

--- src/pages/homepage/Footer.jsx
+++ src/pages/homepage/Footer.jsx
@@ -7,12 +7,12 @@ function Footer() {
       <footer className="py-6">
         <div className="container mx-auto">
           <div className="text-center mt-9">
-            <p className={`${styles.fontWeight} text-3xl uppercase`}>
+            <p className={`${styles.fontWeight} text-lg lg:text-3xl md:text-2xl sm:text-lg uppercase`}>
               For more information please Email at :
             </p>
           </div>
           <div className="text-center mt-5">
-            <p className={`${styles.fontWeight} text-3xl uppercase`}>
+            <p className={`${styles.fontWeight} text-base lg:text-3xl md:text-2xl sm:text-lg uppercase`}>
             sales@sobhalimited.co.in
             </p>
           </div>
